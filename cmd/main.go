499094e3234e854a5796290102faca5515d74e36
Ayabonga <ayabongabooi2@gmail.com>
2023-07-11 20:30:40 +0200

    chore(main): setup app entry point

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,36 @@
 package main
 
-func main()  {
-	
+import (
+	"fmt"
+	"time"
+
+	"github.com/gin-gonic/gin"
+
+	"github.com/mrbooi/event_backend/bootstrap"
+	routes "github.com/mrbooi/event_backend/routes"
+)
+
+func main() {
+
+	app := bootstrap.App()
+
+	env := app.Env
+
+	db := app.Mongo.Database(env.DBName)
+	defer app.CloseDBConnection()
+
+	timeout := time.Duration(env.ContextTimeout)
+
+	gin := gin.Default()
+
+	routes.Setup(env, timeout, db, gin)
+
+	// getup middleware to deserializeUser
+	// gin.Use(deserializeUser)
+
+	err := gin.Run(env.ServerAddress)
+	if err != nil {
+		fmt.Println("Failed while setting up App routes.")
+	}
+
 }
