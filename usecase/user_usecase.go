064d4935c85d19eea86d4b634df03668666ef8ef
Ayabonga <ayabongabooi2@gmail.com>
2023-08-14 16:49:09 +0200

    chore(user_usecase): fix typo

--- usecase/user_usecase.go
+++ usecase/user_usecase.go
@@ -7,39 +7,39 @@ import (
 	"github.com/mrbooi/event_backend/domain"
 )
 
-type userUsecase struct {
+type userUseCase struct {
 	userRepository domain.UserRepository
 	contextTimeout time.Duration
 }
 
 // CreateUser implements domain.UserUserCase.
-func (us *userUsecase) CreateUser(ctx context.Context, user *domain.User) error {
+func (us *userUseCase) CreateUser(ctx context.Context, user *domain.User) error {
 	cxt, cancel := context.WithTimeout(ctx, us.contextTimeout)
 	defer cancel()
 	return us.userRepository.CreateUser(cxt, user)
 }
 
 // FindAndUpdateUser implements domain.UserUserCase.
-func (us *userUsecase) FindAndUpdateUser(ctx context.Context, userId string, userData *domain.User) error {
+func (us *userUseCase) FindAndUpdateUser(ctx context.Context, userId string, userData *domain.User) error {
 	panic("unimplemented")
 }
 
 // FindUser implements domain.UserUserCase.
-func (us *userUsecase) FindUser(ctx context.Context, userId string) (domain.User, error) {
+func (us *userUseCase) FindUser(ctx context.Context, userId string) (domain.User, error) {
 	cxt, cancel := context.WithTimeout(ctx, us.contextTimeout)
 	defer cancel()
 	return us.userRepository.FindUser(cxt, userId)
 }
 
 // ValidateUser implements domain.UserUserCase.
-func (us *userUsecase) ValidateUser(ctx context.Context, email string, uuid string) (domain.User, error) {
+func (us *userUseCase) ValidateUser(ctx context.Context, email string, uuid string) (domain.User, error) {
 	cxt, cancel := context.WithTimeout(ctx, us.contextTimeout)
 	defer cancel()
-	return us.userRepository.ValidateUser(cxt, email,uuid)
+	return us.userRepository.ValidateUser(cxt, email, uuid)
 }
 
-func NewSignupUsecase(userRepository domain.UserRepository, timeout time.Duration) domain.UserUserCase {
-	return &userUsecase{
+func NewSignUpUsecase(userRepository domain.UserRepository, timeout time.Duration) domain.UserUserCase {
+	return &userUseCase{
 		userRepository: userRepository,
 		contextTimeout: timeout,
 	}
